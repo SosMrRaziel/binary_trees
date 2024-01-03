7ec6a19793d2fb40974b897179868cfecc4084dd
SosMrRaziel <lbt_alh@hotmail.fr>
2024-01-03 22:00:15 +0100

    task 10

--- 10-binary_tree_depth.c
+++ 10-binary_tree_depth.c
@@ -1,5 +1,18 @@
 #include "binary_trees.h"
 
+/**
+ * binary_tree_depth - measures the depth of a node in a binary tree
+ * @tree: pointer to the node to measure the depth
+ *
+ * Description: A binary tree is a tree data structure in which each node
+ * has at most two children, which are referred to as the left child and
+ * the right child. The depth of a node is the number of edges from the
+ * node to the tree's root node. A root node will have a depth of 0. An
+ * empty tree has a depth of 0.
+ *
+ * Return: the depth of the node, or 0 if tree is NULL
+ */
+
 size_t binary_tree_depth(const binary_tree_t *tree)
 {
 	/* Base case: empty tree has depth 0 */
